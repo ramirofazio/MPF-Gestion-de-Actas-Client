d51b6a12492a85f4455ac586c1ffc652b0087f63
Ramiro <ramifazio@gmail.com>
2022-09-23 12:41:46 -0300

    Add endpoint to getEfectos and Efecto

---
+++ server/src/controllers/getEfectos.js
@@ -0,0 +1,29 @@
+const getEfectos = require("express").Router();
+const { Efecto } = require("../db");
+
+getEfectos.get("/", async (req, res, next) => {
+  //Todos los efectos
+  try {
+    const efectos = await Efecto.findAll();
+    return res.status(200).send(efectos);
+  } catch (error) {
+    next(error);
+  }
+});
+
+getEfectos.get("/:id", async (req, res) => {
+  //1 solo por ID
+  const efecto_id = req.params.id;
+  try {
+    const efecto = await Efecto.findByPk(efecto_id);
+    if (efecto !== null) {
+      return res.status(200).send(efecto);
+    } else {
+      return res.status(404).send("Efecto not found or not exist");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = getEfectos;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const addActa = require("../controllers/addActa");
 const getActas = require("../controllers/getActas");
 const addEfecto = require("../controllers/addEfecto");
+const getEfectos = require("../controllers/getEfectos");
 
 router.use("/addActa", addActa);
-router.use("/addEfecto", addEfecto);
 router.use("/getActas", getActas);
+router.use("/addEfecto", addEfecto);
+router.use("/getEfectos", getEfectos);
 
 module.exports = router;
