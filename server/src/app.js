c0e318619f8f3d8335665ee69790220d5f89495c
Ramiro <ramifazio@gmail.com>
2022-09-23 15:12:10 -0300

    Add cors

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
-
+const cors = require("cors");
 require("./db.js");
 
 const server = express();
@@ -21,6 +21,7 @@ server.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
+server.use(cors());
 
 server.use("/", routes);
 
