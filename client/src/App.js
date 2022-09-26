ad7206248542f1bb20a7e54c8e1add2877c6aa84
Ramiro <ramifazio@gmail.com>
2022-09-26 14:54:58 -0300

    First test of docx working

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllActas } from "./redux/actions";
 
+//Components
 import Fallback from "./Components/Fallback";
 import NavBar from "./Components/Navbar";
 const Inicio = lazy(() => import("./Components/Inicio"));
@@ -8,6 +11,15 @@ const CrearActa = lazy(() => import("./Components/CrearActa"));
 const Consultas = lazy(() => import("./Components/Consultas"));
 
 function App() {
+  const dispatch = useDispatch();
+
+  const actas = useSelector((state) => state?.allActas);
+
+  useEffect(() => {
+    dispatch(getAllActas());
+  }, []);
+  console.log(actas);
+
   return (
     <Router>
       <Suspense fallback={<Fallback />}>
--- client/src/Components/Inicio.jsx
+++ client/src/Components/Inicio.jsx
@@ -1,18 +1,8 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllActas } from "../redux/actions";
+import React from "react";
 import styled from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 
 function Inicio() {
-  const dispatch = useDispatch();
-
-  const actas = useSelector((state) => state?.allActas);
-
-  useEffect(() => {
-    dispatch(getAllActas());
-  }, []);
-  console.log(actas);
   return (
     <Container>
       <h1>Inicio</h1>
