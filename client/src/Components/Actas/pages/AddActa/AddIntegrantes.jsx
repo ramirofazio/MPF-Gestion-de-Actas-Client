bb2b62458373185757740fcdb8fe15927f0ba899
Ramiro <ramifazio@gmail.com>
2022-10-19 16:17:58 -0300

    Start integrantes

--- client/src/Components/Actas/pages/AddActa/AddIntegrantes.jsx
+++ client/src/Components/Actas/pages/AddActa/AddIntegrantes.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 //* Redux
 import { useSelector } from "react-redux";
 //* Style
@@ -7,12 +7,31 @@ import GlobalStyles from "../../../../Styles/GlobalStyles";
 import Variables from "../../../../Styles/Variables";
 //* Initializations
 const { redColor } = Variables;
-const { enProcesoContainer, header, headerTitle, headerDescription, button } = GlobalStyles;
+const { enProcesoContainer, header, headerTitle, headerDescription, button, formContainer } = GlobalStyles;
 
 function AddIntegrantes() {
   const currentActa = useSelector((state) => state.currentActa);
+  const [integrantes, setIntegrantes] = useState([]);
+  const [integrante, setIntegrante] = useState({
+    nombreYApellido: "",
+    dni: "",
+    legajoOMatricula: "",
+    cargo: "",
+  });
 
-  console.log(currentActa);
+  useEffect(() => {
+    console.log(integrantes);
+  }, [integrantes]);
+
+  const handleClick = () => {
+    setIntegrantes(integrante);
+    setIntegrante({
+      nombreYApellido: "",
+      dni: "",
+      legajoOMatricula: "",
+      cargo: "",
+    });
+  };
 
   return (
     <Container>
@@ -21,6 +40,40 @@ function AddIntegrantes() {
         <Title>Creacion de Suscriptores</Title>
         <Description>Encabezado</Description>
       </Header>
+
+      <FormContainer>
+        <form style={{ display: "flex", flexDirection: "column" }}>
+          <input
+            type="text"
+            name="nombreYApellido"
+            value={integrante.nombreYApellido}
+            placeholder="Nombre y Apellido"
+            onChange={(e) => setIntegrante({ ...integrante, nombreYApellido: e.target.value })}
+          />
+          <input
+            type="text"
+            name="dni"
+            value={integrante.dni}
+            placeholder="DNI"
+            onChange={(e) => setIntegrante({ ...integrante, dni: e.target.value })}
+          />
+          <input
+            type="text"
+            name="legajoOMatricula"
+            value={integrante.legajoOMatricula}
+            placeholder="Legajo o Matricula"
+            onChange={(e) => setIntegrante({ ...integrante, legajoOMatricula: e.target.value })}
+          />
+          <input
+            type="text"
+            name="cargo"
+            value={integrante.cargo}
+            placeholder="Cargo"
+            onChange={(e) => setIntegrante({ ...integrante, cargo: e.target.value })}
+          />
+        </form>
+        <AddButton onClick={() => handleClick()}>Agregar Integrante</AddButton>
+      </FormContainer>
 
       <Button>Siguente</Button>
     </Container>
@@ -46,10 +99,18 @@ const Description = styled.h1`
   ${headerDescription}
 `;
 
+const AddButton = styled.button`
+  ${button}
+`;
+
 const Button = styled.button`
   ${button}
   text-decoration: none;
   background: white;
   border: 2px solid ${redColor};
   pointer-events: none;
+`;
+
+const FormContainer = styled.div`
+  ${formContainer}
 `;
