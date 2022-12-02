092aa55d07eb63e6be0cf13c37914b4378cf8aff
ramiro <ramifazio@gmail.com>
2022-12-02 13:18:01 -0300

    config localServer

--- src/Styles/Variables.js
+++ src/Styles/Variables.js
@@ -1,15 +1,15 @@
 const Variables = {
-	//colors
-	principalColor: "#006473",
-	secondaryColor: "#3B4F5F",
-	baseColor: "#ffffff",
-	baseTransparentColor: "#ffffffb5",
-	yellowColor: "#947513",
-	greenColor: "#6aa84f",
-	redColor: "#a84f4f",
-	grayDisableColor: "#4E696340",
-	//config
-	baseEndpoint: "http://localhost:3030",
+  //colors
+  principalColor: "#006473",
+  secondaryColor: "#3B4F5F",
+  baseColor: "#ffffff",
+  baseTransparentColor: "#ffffffb5",
+  yellowColor: "#947513",
+  greenColor: "#6aa84f",
+  redColor: "#a84f4f",
+  grayDisableColor: "#4E696340",
+  //config
+  baseEndpoint: "http://10.190.12.72:3001",
 };
 
 export default Variables;
