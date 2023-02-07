9dd5d344b971779fa554e01c100eb51e25d02cf8
ramiro <ramifazio@gmail.com>
2023-02-07 13:30:24 -0300

    details

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -198,8 +198,6 @@ const GlobalStyles = {
     &:hover {
       min-height: 15%;
       background-color: ${baseTransparentColor};
-      background-color: ${(props) =>
-        props.estado === "en proceso" ? "#F8E71C35" : props.estado === "completa" ? "#7ED32135" : "#00647335"};
     }
   `,
 
