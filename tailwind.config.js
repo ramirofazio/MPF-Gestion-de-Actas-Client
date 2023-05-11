08310211854655cec0156f1b8c7c794759bbe6ee
ramiro <ramifazio@gmail.com>
2023-05-11 14:38:56 -0300

    Add colors to tailwind file config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      secondary: "#3B4F5F",
+      base: "#ffffff",
+      principal: "#006473",
+      transparent: "transparent",
+      succes: "#68BC00",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
   },
   plugins: [],
 };
