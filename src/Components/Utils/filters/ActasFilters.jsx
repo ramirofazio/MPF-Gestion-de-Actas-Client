9202c0f3e1996706d1eb076866919a651df9ceaa
ramiro <ramifazio@gmail.com>
2023-05-15 14:47:30 -0300

    Filtros migrados a tailwind

--- src/Components/Utils/filters/ActasFilters.jsx
+++ src/Components/Utils/filters/ActasFilters.jsx
@@ -38,75 +38,54 @@ function ActasFilters() {
   };
 
   return (
-    <FilterContainer>
-      <Form onSubmit={handleReset}>
-        <InputContainer>
-          <Label>Nro MPF</Label>
-          <Input type="number" value={filters.mpf} onChange={(e) => setFilters({ ...filters, mpf: e.target.value })} maxLength={12} />
-        </InputContainer>
-        <InputContainer>
-          <Label>Nro CIJ</Label>
-          <Input type="number" value={filters.cij} onChange={(e) => setFilters({ ...filters, cij: e.target.value })} maxLength={12} />
-        </InputContainer>
-        <InputContainer>
-          <Label>Nro DIL</Label>
-          <Input type="number" value={filters.dil} onChange={(e) => setFilters({ ...filters, dil: e.target.value })} maxLength={12} />
-        </InputContainer>
-        <InputContainer>
-          <Label>Estado</Label>
-          <Select value={filters.estado} onChange={(e) => setFilters({ ...filters, estado: e.target.value })}>
+    <>
+      <form className="ml-5 flex w-[50%] justify-around self-start">
+        <div className="filterInputContainer">
+          <label className="basicLabel">Nro MPF</label>
+          <input
+            className="filterInput"
+            type="number"
+            value={filters.mpf}
+            onChange={(e) => setFilters({ ...filters, mpf: e.target.value })}
+            maxLength={12}
+          />
+        </div>
+        <div className="filterInputContainer">
+          <label className="basicLabel">Nro CIJ</label>
+          <input
+            className="filterInput"
+            type="number"
+            value={filters.cij}
+            onChange={(e) => setFilters({ ...filters, cij: e.target.value })}
+            maxLength={12}
+          />
+        </div>
+        <div className="filterInputContainer">
+          <label className="basicLabel">Nro DIL</label>
+          <input
+            className="filterInput"
+            type="number"
+            value={filters.dil}
+            onChange={(e) => setFilters({ ...filters, dil: e.target.value })}
+            maxLength={12}
+          />
+        </div>
+        <div className="filterInputContainer">
+          <label className="basicLabel">Estado</label>
+          <select className="filterInput" value={filters.estado} onChange={(e) => setFilters({ ...filters, estado: e.target.value })}>
             <option value="">Todas</option>
             <option value="en proceso">En Proceso</option>
             <option value="completa">Completas</option>
             <option value="en creacion">En Creacion</option>
             <option value="para completar">Para completar</option>
-          </Select>
-        </InputContainer>
-        <InputContainer
-          style={{
-            justifyContent: "flex-end",
-            alignItems: "flex-start",
-          }}
-        >
-          <Submit type="submit" />
-          <RestoreIcon />
-        </InputContainer>
-      </Form>
-    </FilterContainer>
+          </select>
+        </div>
+        <div className="flex w-24 items-center">
+          <SettingsBackupRestore className="transition hover:cursor-pointer hover:text-secondary" size={25} onClick={handleReset} />
+        </div>
+      </form>
+    </>
   );
 }
 
 export default ActasFilters;
-
-const FilterContainer = styled.div`
-  ${filtersContainer}
-`;
-
-const Form = styled.form`
-  display: flex;
-`;
-
-const InputContainer = styled.div`
-  ${filtersInputContainer}
-`;
-
-const Label = styled.label`
-  ${label}
-`;
-
-const Input = styled.input`
-  ${filtersInput}
-`;
-
-const Select = styled.select`
-  ${filtersInput}
-`;
-
-const Submit = styled.input`
-  ${submitBtn}
-`;
-
-const RestoreIcon = styled(SettingsBackupRestore)`
-  width: 20%;
-  color: ${secondaryColor};
-`;
