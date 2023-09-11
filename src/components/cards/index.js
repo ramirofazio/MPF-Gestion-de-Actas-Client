007b16a9d3fe3a541fc7c0300ee04ee82188668e
Ramiro <ramifazio@gmail.com>
2023-09-11 15:40:51 -0300

    Add bolsa Card and some refactor

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -2,3 +2,4 @@ export { ActaCard } from "./ActaCard";
 export { ActasCards } from "./ActasCards";
 export { EfectosCard } from "./EfectosCard";
 export { CardElement } from "./CardElement";
+export { BolsaCard } from "./BolsaCard";
--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export { NavBar } from "./NavBar";
 export { Fallback } from "./Fallback";
 export { NotFound } from "./NotFound";
 export { ActasFilters } from "./ActasFilters";
-export { ActaCard, ActasCards, EfectosCard, CardElement } from "./cards";
+export { ActaCard, ActasCards, EfectosCard, CardElement, BolsaCard } from "./cards";
 export {
   Input,
   Select,
