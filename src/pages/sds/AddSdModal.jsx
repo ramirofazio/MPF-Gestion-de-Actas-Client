23b50a742d73835b3087068641f9e76d6a36fde2
Ramiro <ramifazio@gmail.com>
2023-09-19 13:31:02 -0300

    Refactor SD modal

--- src/pages/sds/AddSdModal.jsx
+++ src/pages/sds/AddSdModal.jsx
@@ -1,3 +1,4 @@
+import { Input, Select, TipoExtracciones } from "components/fields";
 import React from "react";
 
 export function AddSdModal({ sds, setSds, setAddSdModal, toast }) {
@@ -5,7 +6,7 @@ export function AddSdModal({ sds, setSds, setAddSdModal, toast }) {
     return () => {
       localStorage.setItem("currentSd", null);
     };
-  });
+  }, []);
 
   const [sd, setSd] = React.useState(
     JSON.parse(localStorage.getItem("currentSd")) || {
@@ -39,59 +40,34 @@ export function AddSdModal({ sds, setSds, setAddSdModal, toast }) {
 
   return (
     <>
-      <header className="modalHeader">
-        <span data-aos="fade-down">{sd.edit ? "Editar Sd" : "Agregar Sd"}</span>
+      <header className="modalHeader" data-aos="fade-down">
+        {sd.edit ? "Editar Sd" : "Agregar Sd"}
       </header>
       <form data-aos="zoom-in" className="flex w-full flex-col justify-center p-5 pt-0" onSubmit={handleSdSubmit}>
-        <div className="modalInputContainer">
-          <label className="basicLabel !text-white">Marca</label>
-          <input
-            className="formModalInput"
-            type="text"
-            name="marca"
-            value={sd.marca}
-            placeholder="Marca"
-            onChange={(e) => setSd({ ...sd, marca: e.target.value.toUpperCase() })}
-          />
-        </div>
-        <div className="modalInputContainer">
-          <label className="basicLabel !text-white">Serial N°</label>
-          <input
-            className="formModalInput"
-            type="text"
-            name="serialNumber"
-            value={sd.serialNumber}
-            placeholder="Serial N°"
-            onChange={(e) => setSd({ ...sd, serialNumber: e.target.value.toUpperCase() })}
-          />
-        </div>
-        <div className="modalInputContainer">
-          <label className="basicLabel !text-white">*Almacenamiento</label>
-          <input
-            className="formModalInput"
-            type="text"
-            name="almacenamiento"
-            value={sd.almacenamiento}
-            placeholder="500 GB / 1 TB"
-            onChange={(e) => setSd({ ...sd, almacenamiento: e.target.value.toUpperCase() })}
-          />
-        </div>
-        <div className="modalInputContainer">
-          <label className="basicLabel !text-white">*Extracción</label>
-          <select
-            className="formModalSelect"
-            value={sd.tipoExtraccionSd}
-            onChange={(e) => setSd({ ...sd, tipoExtraccionSd: e.target.value })}
-          >
-            <option value="">Tipo de Extracción</option>
-            <option value="ninguna">Ninguna</option>
-            <option value="en proceso">En Proceso</option>
-            <option value="fisica">Fisica</option>
-            <option value="lógica ">Logica</option>
-            <option value="sistema de archivos">Sistema de Archivos</option>
-            <option value="lógica  avanzada">Logica Avanzada</option>
-          </select>
-        </div>
+        <Input
+          label={"Marca"}
+          value={sd.marca}
+          placeholder="Marca"
+          onChange={(e) => setSd({ ...sd, marca: e.target.value.toUpperCase() })}
+        />
+        <Input
+          label={"Serial N°"}
+          value={sd.serialNumber}
+          placeholder="Serial N°"
+          onChange={(e) => setSd({ ...sd, serialNumber: e.target.value.toUpperCase() })}
+        />
+        <Input
+          label={"*Almacenamiento"}
+          value={sd.almacenamiento}
+          placeholder="500 GB / 1 TB"
+          onChange={(e) => setSd({ ...sd, almacenamiento: e.target.value.toUpperCase() })}
+        />
+        <Select
+          label={"*Extracción"}
+          options={<TipoExtracciones />}
+          value={sd.tipoExtraccionSd}
+          onChange={(e) => setSd({ ...sd, tipoExtraccionSd: e.target.value })}
+        />
         <div className="mt-2 flex w-full items-center justify-around">
           <input className="submitBtn " type="submit" value="Guardar" />
         </div>
