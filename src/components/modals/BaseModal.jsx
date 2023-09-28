f1a4fdccbccdb562ddc268cc50d399e94af1cd94
Ramiro <ramifazio@gmail.com>
2023-09-28 13:26:58 -0300

    hot fix close icon z-index

--- src/components/modals/BaseModal.jsx
+++ src/components/modals/BaseModal.jsx
@@ -6,11 +6,11 @@ export function BaseModal({ isOpen, close, content }) {
     <Modal
       isOpen={isOpen}
       ariaHideApp={false}
-      className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50"
+      className="absolute inset-0 z-20 flex items-center justify-center bg-black bg-opacity-70"
       overlayClassName="bg-black bg-opacity-50"
     >
       <div className="bgTools relative w-[40%] rounded-md">
-        <I.close className="closeModalIcon absolute right-2 top-2 cursor-pointer" onClick={() => close(false)} />
+        <I.close className="closeModalIcon absolute right-2 top-2 z-10 cursor-pointer" onClick={() => close(false)} />
         <div data-aos="zoom-in" className="w-full">
           {content}
         </div>
